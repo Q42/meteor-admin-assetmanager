6f390f2b96d0580233eefbadd1c1dc35de6852d9
Jeroen Gijsman <jeroen@q42.nl>
2015-06-05 11:08:07 +0200

    generic collection name

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'q42:meteor-admin-assetmanager',
-  version: '0.1.1',
+  version: '0.1.2',
 
   summary: 'Asset manager for Meteor Admin',
   git: 'https://github.com/Q42/meteor-admin-assetmanager',

612872211d104019681f5319da2cf0f4ffca03f5
Christiaan Hees <mail@chees.info>
2015-07-17 12:23:52 +0200

    Version bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'q42:meteor-admin-assetmanager',
-  version: '0.1.6',
+  version: '0.1.7',
 
   summary: 'Asset manager for Meteor Admin',
   git: 'https://github.com/Q42/meteor-admin-assetmanager',

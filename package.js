018fcea7b4651ad3c6838c45bb3514e3c5efb39d
Remco Veldkamp <remcoder@gmail.com>
2015-07-20 17:49:34 +0200

    bump version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'q42:meteor-admin-assetmanager',
-  version: '0.2.1',
+  version: '0.3.0',
 
   summary: 'Asset manager for Meteor Admin',
   git: 'https://github.com/Q42/meteor-admin-assetmanager',
